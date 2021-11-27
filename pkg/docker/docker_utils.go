d0695d67ecf7181bef7155a4eb1e88162f9b0df3
Jane Xing <zhouxing@uchicago.edu>
2021-11-27 13:11:38 -0600

    demo passed
    Release note (<category, see below>): <what> <show> <why>

--- pkg/docker/docker_utils.go
+++ pkg/docker/docker_utils.go
@@ -1,242 +1,319 @@
 package docker
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"math"
+	"os"
 	"os/exec"
+	"regexp"
 	"strings"
-	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
+	"golang.org/x/net/context"
 )
 
 const (
-	FifoFile                 = "new_fifo"
-	buildContainerTimeout    = 300
-	startContainerTimeout    = 20
-	stopContainerTimeout     = 300
-	cpFileToContainerTimeout = 30
+	imageName           = "cockroachdb/jane_cockroach:latest"
+	defaultTimeout      = 10
+	serverStartTimeout  = 20
+	listenUrlFile       = "demoFile"
+	cockroachEntrypoint = "./cockroach"
+	HostPort            = "8080"
+	CockroachPort       = "26275"
+	HostIP              = "127.0.0.1"
 )
 
-// removeAllContainers stops and removes all existing containers.
-func removeAllContainers() error {
-	_, err := exec.Command(
-		"sh",
-		"-c",
-		"docker",
-		"container",
-		"ls",
-		"-a",
-		"-q",
-		"|",
-		"xargs",
-		"docker",
-		"container",
-		"rm",
-		"-f",
-	).Output()
+type dockerNode struct {
+	cli    client.APIClient
+	contId string
+}
+
+// cleanUp removes existing database and the log file.
+func cleanUp() error {
+	_, err := exec.Command("rm", "-rf", "./cockroach-data").Output()
 	if err != nil {
-		return fmt.Errorf("failed to remove all containers: %v", err)
+		return fmt.Errorf("cannot cleanup: %v", err)
 	}
 	return nil
 }
 
-// listContainers print the list of all active containers.
-func listContainers() (string, error) {
-	output, err := exec.Command("docker", "container", "ls").Output()
+// showContainerLog outputs the container's logs to the given io.Writer.
+func (dn *dockerNode) showContainerLog(ctx context.Context, logFileName string) error {
+
+	cmdLog, err := os.Create(logFileName)
+	out := io.MultiWriter(cmdLog, os.Stderr)
+
+	rc, err := dn.cli.ContainerLogs(ctx, dn.contId, types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+	})
 	if err != nil {
-		return "", fmt.Errorf("failed tp list all active docker containers: %v",
-			err)
+		return err
 	}
-	return string(output), nil
-}
-
-// logContainer prints the logs of a container. This is a helper function the
-// server fails to start a container.
-func logContainer(containerId string) error {
-	output, err := exec.Command("docker", "logs", containerId).Output()
-	if err != nil {
-		return fmt.Errorf("failed to show log of container: %v", err)
-	}
-
-	fmt.Println(string(output))
-	return nil
-}
-
-// buildDockerContainer builds the docker container with containerName from a given
-// docker image specified in dockerRunArgs.
-func buildDockerContainer(dockerRunArgs []string, containerName string) error {
-	// Must make sure that the container name is consistent with the one
-	// in dockerRunArgs.
-	dockerRunArgs = append(dockerRunArgs, fmt.Sprintf("--listening-url-file=%s", FifoFile))
-
-	if len(dockerRunArgs) == 0 {
-		return fmt.Errorf("args for docker run failed to be empty")
-	}
-
-	if dockerRunArgs[0] != "run" {
-		return fmt.Errorf("the first arg of dockerRunArgs must be \"run\"")
-	}
-
-	output, err := exec.Command(
-		"docker",
-		dockerRunArgs...,
-	).Output()
-	if err != nil {
-		return fmt.Errorf("failed to run the docker: %s", err)
-	}
-
-	containerId := strings.TrimSpace(string(output))
-
-	cpFinished := make(chan error)
-
-	go func() {
-		_, err := exec.Command("docker", "cp", "./read_fifo.sh",
-			fmt.Sprintf("%s:cockroach/", containerName)).Output()
-		if err != nil {
-			cpFinished <- fmt.Errorf("cannot copy read_fifo to container: %v", err)
-			return
-		}
-		close(cpFinished)
-	}()
-
-	cpFileTimeout := time.After(cpFileToContainerTimeout * time.Second)
-
-	select {
-	case <-cpFileTimeout:
-		return fmt.Errorf("copy read_fifo to container timeout")
-	case err := <-cpFinished:
-		if err != nil {
+	defer rc.Close()
+	// The docker log output is not quite plaintext: each line has a
+	// prefix consisting of one byte file descriptor (stdout vs stderr),
+	// three bytes padding, four byte length. We could use this to
+	// disentangle stdout and stderr if we wanted to output them into
+	// separate streams, but we don't really care.
+	for {
+		var header uint64
+		if err := binary.Read(rc, binary.BigEndian, &header); err == io.EOF {
+			break
+		} else if err != nil {
 			return err
 		}
-		fmt.Println("successfully copied the file!")
-	}
-
-	timeout := time.Second * startContainerTimeout
-	for start := time.Now(); ; {
-		if time.Since(start) > timeout {
-			if err := logContainer(containerId); err != nil {
-				return fmt.Errorf("error to check log for container %s: %v",
-					containerId, err)
-			}
-			return fmt.Errorf("timeout for container %s to start", containerId)
-		}
-
-		containerList, err := listContainers()
-		if err != nil {
+		size := header & math.MaxUint32
+		if _, err := io.CopyN(out, rc, int64(size)); err != nil {
 			return err
-		}
-
-		if strings.Contains(containerList, containerName) {
-			fmt.Printf("container %s is running", containerId)
-			break
-		}
-	}
-
-	//time.Sleep(1 * time.Second)
-
-	// Check if the cockroach server is started by checking if the fifo is passed
-	// with the url.
-	type outputStruct struct {
-		output string
-		err    error
-	}
-
-	finished := make(chan outputStruct)
-	go func() {
-		output, err = exec.Command(
-			"sh",
-			"-c",
-			"docker",
-			"exec",
-			"-i",
-			containerName,
-			"./read_fifo.sh",
-			FifoFile,
-			"30",
-		).Output()
-		finished <- outputStruct{string(output), err}
-	}()
-	timeout = time.Second * buildContainerTimeout
-	select {
-	case <-time.After(timeout):
-		return fmt.Errorf("timeout for building container")
-	case commandOutput := <-finished:
-		if commandOutput.err != nil {
-			return fmt.Errorf("failed reading fifo file: %v", err)
-		}
-		fmt.Printf("successfully built container, "+
-			"with cockroach single node running on %s \n", output)
-
-		return nil
-	}
-}
-
-// executeSqlQuery executes the sql query in a given container,
-// and returns the output of this query and an error.
-func executeSqlQuery(
-	query string, containerName string, cockroachSqlArgs []string,
-) (string, error) {
-	dockerExecCommand := append([]string{
-		"exec",
-		"-i",
-		containerName,
-		"./cockroach",
-		"sql",
-		"-e",
-		fmt.Sprintf("%s;", query),
-	}, cockroachSqlArgs...)
-
-	cmd := exec.Command(
-		"docker",
-		dockerExecCommand...,
-	)
-	stdout, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to execute sql query %s in docker container"+
-			" %s"+
-			": %v"+
-			"", query, containerName, err)
-	}
-	// Return the output of sql query.
-	return string(stdout), nil
-}
-
-// removeContainer removes a container,
-// and returns an error if it failed to be removed or the removal times out.
-func removeContainer(containerName string) error {
-	cmd := exec.Command(
-		"docker",
-		"rm",
-		"-f",
-		containerName,
-	)
-	_, err := cmd.Output()
-	if err != nil {
-		return fmt.Errorf("failed to remove a container %s: %v",
-			containerName,
-			err)
-	}
-
-	var output []byte
-	timeout := time.Second * stopContainerTimeout
-	for start := time.Now(); ; {
-		if time.Since(start) > timeout {
-			return fmt.Errorf("timeout for removing container %s", containerName)
-		}
-		// Check if the container is truly removed.
-		output, err = exec.Command(
-			"sh",
-			"-c",
-			"docker",
-			"container",
-			"ls",
-			"-a",
-			"|",
-			"grep",
-			containerName,
-		).Output()
-		if len(output) == 0 {
-			fmt.Printf("docker container %s is fully stopped \n", containerName)
-			break
 		}
 	}
 	return nil
+}
+
+// startContainer starts a container with given setting for environment
+// variables, mounted volumes, and command to run.
+func (dn *dockerNode) startContainer(
+	ctx context.Context, containerName string, envSetting []string, volSetting []string, cmd []string,
+) error {
+
+	containerConfig := container.Config{
+		Hostname:     containerName,
+		Image:        imageName,
+		Env:          envSetting,
+		ExposedPorts: nat.PortSet{HostPort: struct{}{}, CockroachPort: struct{}{}},
+		Cmd:          append(cmd, fmt.Sprintf("--listening-url-file=%s", listenUrlFile)),
+	}
+
+	hostConfig := container.HostConfig{
+		Binds: volSetting,
+		PortBindings: map[nat.Port][]nat.PortBinding{
+			nat.Port(HostPort):      {{HostIP: HostIP, HostPort: HostPort}},
+			nat.Port(CockroachPort): {{HostIP: HostIP, HostPort: CockroachPort}},
+		},
+	}
+
+	resp, err := dn.cli.ContainerCreate(
+		ctx,
+		&containerConfig,
+		&hostConfig,
+		nil,
+		nil,
+		containerName,
+	)
+	if err != nil {
+		return fmt.Errorf("cannot create container: %v", err)
+	}
+	fmt.Println("id:", resp.ID)
+
+	dn.contId = resp.ID
+
+	if err := dn.cli.ContainerStart(ctx, dn.contId, types.ContainerStartOptions{}); err != nil {
+		return fmt.Errorf("cannot start container: %v", err)
+	}
+
+	return nil
+}
+
+func (dn *dockerNode) removeAllContainers(ctx context.Context) error {
+	conts, err := dn.cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		return fmt.Errorf("cannot list all conts: %v", err)
+	}
+	for _, cont := range conts {
+		err := dn.cli.ContainerRemove(ctx, cont.ID, types.ContainerRemoveOptions{Force: true})
+		if err != nil {
+			return fmt.Errorf("cannot remove cont %s: %v", cont.Names, err)
+		}
+	}
+	return nil
+}
+
+type ExecResult struct {
+	StdOut   string
+	StdErr   string
+	ExitCode int
+}
+
+func (dn *dockerNode) InspectExecResp(ctx context.Context, execId string) (ExecResult, error) {
+	var execResult ExecResult
+	resp, err := dn.cli.ContainerExecAttach(ctx, execId, types.ExecStartCheck{})
+	if err != nil {
+		return execResult, err
+	}
+	defer resp.Close()
+
+	// read the output
+	var outBuf, errBuf bytes.Buffer
+	outputDone := make(chan error)
+
+	go func() {
+		// StdCopy demultiplexes the stream into two buffers
+		_, err = stdcopy.StdCopy(&outBuf, &errBuf, resp.Reader)
+		outputDone <- err
+	}()
+
+	select {
+	case err := <-outputDone:
+		if err != nil {
+			return execResult, err
+		}
+		break
+
+	case <-ctx.Done():
+		return execResult, ctx.Err()
+	}
+
+	stdout, err := ioutil.ReadAll(&outBuf)
+	if err != nil {
+		return execResult, err
+	}
+	stderr, err := ioutil.ReadAll(&errBuf)
+	if err != nil {
+		return execResult, err
+	}
+
+	res, err := dn.cli.ContainerExecInspect(ctx, execId)
+	if err != nil {
+		return execResult, err
+	}
+
+	execResult.ExitCode = res.ExitCode
+	execResult.StdOut = string(stdout)
+	execResult.StdErr = string(stderr)
+	return execResult, nil
+}
+
+//
+func (dn *dockerNode) execCommand(ctx context.Context, cmd []string) (*ExecResult, error) {
+	execId, err := dn.cli.ContainerExecCreate(ctx, dn.contId, types.ExecConfig{
+		AttachStderr: true,
+		AttachStdout: true,
+		Tty:          true,
+		Cmd:          cmd,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf(
+			"cannot create command \"%s\":\n %v",
+			strings.Join(cmd[:], " "),
+			err,
+		)
+	}
+
+	res, err := dn.InspectExecResp(ctx, execId.ID)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"cannot execute command \"%s\":\n %v",
+			strings.Join(cmd[:], " "),
+			err,
+		)
+	}
+
+	if res.ExitCode != 0 {
+		return &res, fmt.Errorf(
+			"command \"%s\" exit with code %d:\n %+v",
+			strings.Join(cmd[:], " "),
+			res.ExitCode,
+			res)
+	}
+
+	return &res, nil
+}
+
+// waitServerStarts waits till the server truly starts or timeout, whichever
+// earlier. It keeps listening to the
+func (dn *dockerNode) waitServerStarts(ctx context.Context) error {
+	// Note that at startContainer we explicitly passed listenUrlFile to
+	// --listening-url-file when starting a single node server.
+	// When this listenUrlFile is written and closed,
+	// it means the server has fully started.
+	// We use inotifywait to wait for this event to occur or timeout, whichever
+	// is earlier. If this file is not written and closed within timeout,
+	// it gives a timeout error.
+	res, err := dn.execCommand(
+		ctx,
+		[]string{
+			"inotifywait",
+			"-e",
+			"close_write",
+			"-t",
+			fmt.Sprint(serverStartTimeout),
+			"--include",
+			listenUrlFile,
+			"."},
+	)
+	if err != nil {
+		if res == nil {
+			return err
+		}
+		switch res.ExitCode {
+		// If inotifywait exit with code 2, it means the event didn't occur within timeout.
+		case 2:
+			return fmt.Errorf("timeout for starting the server")
+		case 137:
+			return fmt.Errorf("%v\nHINT: it could be because the init script is"+
+				" erronous, \nand made the container to exit. \n"+
+				"Please double check the init scripts have the correct commands.", err)
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+// execSqlQuery executes the sql query and returns the server's output and
+// possible error.
+func (dn *dockerNode) execSqlQuery(
+	ctx context.Context, sqlQuery string, sqlQueryOpts []string,
+) (*ExecResult, error) {
+	query := append([]string{cockroachEntrypoint, "sql", "-e", fmt.Sprintf("%s", sqlQuery)},
+		sqlQueryOpts...,
+	)
+
+	res, err := dn.execCommand(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("error executing query \"%s\": \n%v", sqlQuery, err)
+	}
+
+	return res, nil
+}
+
+// rmContainer does a forced remove of the current container.
+func (dn *dockerNode) rmContainer(ctx context.Context) error {
+	if err := dn.cli.ContainerRemove(ctx, dn.contId, types.ContainerRemoveOptions{
+		Force: true,
+	}); err != nil {
+		return fmt.Errorf("cannot remove container %s: %v", dn.contId, err)
+	}
+	return nil
+}
+
+// cleanSqlOutput format the output of sql query to be easier to compare
+// with the expected result.
+func cleanSqlOutput(s string) string {
+	trimDash := regexp.MustCompile(`(\n|^)(\-|\+){3,}`)
+	trimNewline := regexp.MustCompile(`(\r|\n){2,}`)
+	trimTime := regexp.MustCompile(`\nTime:(.|\s)+`)
+	trimRow := regexp.MustCompile(`\n\(\d+ rows?\)`)
+	trimSpace := regexp.MustCompile(`\n\s{1,}`)
+
+	var res string
+
+	res = strings.TrimSpace(s)
+	res = trimDash.ReplaceAllString(res, "")
+	res = trimNewline.ReplaceAllString(res, "\n")
+	res = trimTime.ReplaceAllString(res, "")
+	res = trimRow.ReplaceAllString(res, "")
+	res = trimSpace.ReplaceAllString(res, "\n")
+
+	return res
 }
