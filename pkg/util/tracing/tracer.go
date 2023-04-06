332bc87b809db1394128e6eecd912ff5bb3e0e12
Jane Xing <zhouxing@uchicago.edu>
2023-04-06 10:18:47 -0400

    set trace to normal

--- pkg/util/tracing/tracer.go
+++ pkg/util/tracing/tracer.go
@@ -193,11 +193,12 @@ var periodicSnapshotInterval = settings.RegisterDurationSetting(
 
 // panicOnUseAfterFinish, if set, causes use of a span after Finish() to panic
 // if detected.
-var panicOnUseAfterFinish = true
+var panicOnUseAfterFinish = buildutil.CrdbTestBuild ||
+	envutil.EnvOrDefaultBool("COCKROACH_CRASH_ON_SPAN_USE_AFTER_FINISH", false)
 
 // debugUseAfterFinish controls whether to debug uses of Span values after finishing.
 // FOR DEBUGGING ONLY. This will slow down the program.
-var debugUseAfterFinish = true
+var debugUseAfterFinish = envutil.EnvOrDefaultBool("COCKROACH_DEBUG_SPAN_USE_AFTER_FINISH", false)
 
 // reuseSpans controls whether spans can be re-allocated after they've been
 // Finish()ed. See Tracer.spanReusePercent for details.
